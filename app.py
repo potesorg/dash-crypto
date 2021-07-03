1afd000ad7291f1b83faba313eb097f62bae7bb2
andreituta <tuta.andrei96@gmail.com>
2021-07-04 00:13:10 +0300

    Add line graph for each coin

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import dash_html_components as html
 # plotting
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.express as px
 
 
 app = dash.Dash()
@@ -33,71 +34,71 @@ class Preferences():
         """
 
 
-def write_responses(coins, coin):
-    with open('results/'+coin+'.csv', 'w') as csvfile:
-        # creating a csv writer object
-        csvwriter = csv.writer(csvfile)
+class MessariHandler():
+    def __init__(self, prefs: Preferences):
+        # init an empty df to store the data
+        self.all_df = pd.DataFrame(
+            columns=['coin', 'symbol', 'high_price', 'timestamp', 'volume'])
+        coins = pref.coins
+        # loop over coins and fetch results
+        for coin, items in coins.items():
+            # self.call(coins, coin, pref. start, pref.end, pref.headers)
+            # path = self.write_responses(coins, coin)
+            # use local csvs to avoid doing API calls
+            path = 'results/'+coin+'.csv'
+            df = pd.read_csv(path)
+            print(type(df), df)
+            # extend the dataframe to include new fetched data
+            self.all_df = self.all_df.append(df, ignore_index=True)
+        pass
 
-        # # writing the fields
-        csvwriter.writerow(
-            ['coin', 'symbol', 'high_price', 'timestamp', 'volume'])
-        # writing the data rows
-        csvwriter.writerows(coins[coin])
-    print("Finished writing for " + coin)
-    return 'results/'+coin+'.csv'
+    def write_responses(self, coins, coin):
+        with open('results/'+coin+'.csv', 'w') as csvfile:
+            # creating a csv writer object
+            csvwriter = csv.writer(csvfile)
 
-    """Makes a call to the Messari api to get the latest data for the period provided
-    """
+            # # writing the fields
+            csvwriter.writerow(
+                ['coin', 'symbol', 'high_price', 'timestamp', 'volume'])
+            # writing the data rows
+            csvwriter.writerows(coins[coin])
+        print("Finished writing for " + coin)
+        return 'results/'+coin+'.csv'
 
+        """Makes a call to the Messari api to get the latest data for the period provided
+        """
 
-def call(coins, coin, start, end, headers):
-    url = "https://data.messari.io/api/v1/assets/" + coin + \
-        "/metrics/price/time-series?start="+start+"&end="+end+"&interval=1d"
-    news_url = "https://data.messari.io/api/v1/news/" + coin
-    print("Initialising for " + coin.upper() + ": " + url)
-    response = requests.get(url, headers=headers)
-    #  this loads news for the current coin
-    # response_news = requests.get(news_url)
-    data = response.json()
-    values = data['data']['values']
-    for value in values:
-        timestamp = value[0]
-        open_price = round(value[1], 2)
-        high_price = round(value[2], 3)
-        low_price = round(value[3], 2)
-        close_price = round(value[4], 2)
-        volume = round(value[5], 2)
-        name = data['data']['name']
-        date = datetime.datetime.fromtimestamp(
-            timestamp/1000.0).strftime("%Y-%m-%d")
-        coin_loop = [name, coin.upper(), high_price, date, volume]
-        print(f"{date} : {volume}@{high_price} for {coin}-{name} prices fetched")
-        coins[coin].append(coin_loop)
+    def call(self, coins, coin, start, end, headers):
+        url = "https://data.messari.io/api/v1/assets/" + coin + \
+            "/metrics/price/time-series?start="+start+"&end="+end+"&interval=1d"
+        news_url = "https://data.messari.io/api/v1/news/" + coin
+        print("Initialising for " + coin.upper() + ": " + url)
+        response = requests.get(url, headers=headers)
+        #  this loads news for the current coin
+        # response_news = requests.get(news_url)
+        data = response.json()
+        values = data['data']['values']
+        for value in values:
+            timestamp = value[0]
+            open_price = round(value[1], 2)
+            high_price = round(value[2], 3)
+            low_price = round(value[3], 2)
+            close_price = round(value[4], 2)
+            volume = round(value[5], 2)
+            name = data['data']['name']
+            date = datetime.datetime.fromtimestamp(
+                timestamp/1000.0).strftime("%Y-%m-%d")
+            coin_loop = [name, coin.upper(), high_price, date, volume]
+            print(f"{date} : {volume}@{high_price} for {coin}-{name} prices fetched")
+            coins[coin].append(coin_loop)
 
 
 pref = Preferences()
-# init an empty df to store the data
-all_df = pd.DataFrame(
-    columns=['coin', 'symbol', 'high_price', 'timestamp', 'volume'])
-coins = pref.coins
-# loop over coins and fetch results
-for coin, items in coins.items():
-    call(coins, coin, pref. start, pref.end, pref.headers)
-    path = write_responses(coins, coin)
-    # use local csvs to avoid doing API calls
-    # path = 'results/'+coin+'.csv'
-    df = pd.read_csv(path)
-    print(type(df), df)
-    # extend the dataframe to include new fetched data
-    all_df = all_df.append(df, ignore_index=True)
-
-print(all_df)
-df = all_df
-
-
-app.layout = html.Div([
+handler = MessariHandler(pref)
+df = handler.all_df
+ch = [
     dcc.Graph(
-        id='life-exp-vs-gdp',
+        id='value-over-time',
         figure={
             'data': [
                 go.Scatter(
@@ -111,7 +112,7 @@ app.layout = html.Div([
                         'line': {'width': 0.5, 'color': 'white'}
                     },
                     name=i
-                ) for i in all_df["coin"].unique()
+                ) for i in df["coin"].unique()
             ],
             'layout': go.Layout(
                 yaxis={'type': 'date', 'title': 'Timestamp'},
@@ -122,6 +123,22 @@ app.layout = html.Div([
             )
         }
     )
-])
+]
+for coin in pref.coins:
+    adf = df[df["symbol"] == coin.upper()]
+    ch.append(
+        dcc.Graph(
+            id=f'value-over-time-{coin}',
+            figure=px.line(
+                adf,
+                x='timestamp',
+                y='high_price'
+            )
+        )
+    )
+
+app.layout = html.Div(children=ch)
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
